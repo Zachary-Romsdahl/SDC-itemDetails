8fb84f7e9e34308785473271d51abddd835f3078
Zachary Romsdahl <zdromsdahl@gmail.com>
2020-11-14 16:17:43 -0800

    Removing Node Cluster

--- server/cluster.js
+++ server/cluster.js
@@ -1,19 +1,21 @@
-const cpus = require('os').cpus();
-const cluster = require('cluster');
+// Commented out for EC2 instances
+// const cpus = require('os').cpus();
+// const cluster = require('cluster');
 
-const numCpus = cpus.length;
-if (cluster.isMaster) {
-  for (let i = 0; i < numCpus; i += 1) {
-    cluster.fork();
-  }
-  cluster.on('online', (worker) => {
-    console.log(`Worker${worker.process.pid}started`);
-  });
-  // If a worker died
-  cluster.on('exit', (worker, code, signal) => {
-    console.log('Worker' + worker.process.pid + 'died with code:' + code + ', and signal:' + signal);
-    console.log('Starting a new worker');
-  });
-} else {
-  require('./start.js');
-}
+// const numCpus = cpus.length;
+// if (cluster.isMaster) {
+//   for (let i = 0; i < numCpus; i += 1) {
+//     cluster.fork();
+//   }
+//   cluster.on('online', (worker) => {
+//     console.log(`Worker ${worker.process.pid} started`);
+//   });
+//   // If a worker died
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`Worker${worker.process.pid} died with code:${code}, and signal: ${signal}`);
+//     console.log('Starting a new worker');
+//   });
+// } else {
+//   // If it is a worker:
+//   require('./start.js');
+// }
